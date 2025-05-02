8ed5f95766c10564f10858f383b9d197c50c5883
Sakala85 <melchiorbengtsson@gmail.com>
2025-05-02 12:42:52 +0200

    Initial Commit

---
+++ abconsent/abconsentApp.swift
@@ -0,0 +1,17 @@
+//
+//  abconsentApp.swift
+//  abconsent
+//
+//  Created by Melchior Bengtsson on 02/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct abconsentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ abconsentTests/abconsentTests.swift
@@ -0,0 +1,17 @@
+//
+//  abconsentTests.swift
+//  abconsentTests
+//
+//  Created by Melchior Bengtsson on 02/05/2025.
+//
+
+import Testing
+@testable import abconsent
+
+struct abconsentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
